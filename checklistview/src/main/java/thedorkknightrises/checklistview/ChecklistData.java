24bae943552baf5bb634de47397b5b57db1f2c52
Samriddha Basu <samriddhabasu@gmail.com>
2017-08-08 01:25:36 +0530

    ChecklistData class is now Parcelable, to allow storing the checklist data array list to be stored in Bundle

--- checklistview/src/main/java/thedorkknightrises/checklistview/ChecklistData.java
+++ checklistview/src/main/java/thedorkknightrises/checklistview/ChecklistData.java
@@ -1,16 +1,36 @@
 package thedorkknightrises.checklistview;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Samriddha on 06-08-2017.
  */
 
-public class ChecklistData {
+public class ChecklistData implements Parcelable {
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<ChecklistData> CREATOR = new Parcelable.Creator<ChecklistData>() {
+        @Override
+        public ChecklistData createFromParcel(Parcel in) {
+            return new ChecklistData(in);
+        }
+
+        @Override
+        public ChecklistData[] newArray(int size) {
+            return new ChecklistData[size];
+        }
+    };
     private boolean checked;
     private String text;
 
     public ChecklistData(boolean checked, String text) {
         this.checked = checked;
         this.text = text;
+    }
+
+    protected ChecklistData(Parcel in) {
+        checked = in.readByte() != 0x00;
+        text = in.readString();
     }
 
     public boolean isChecked() {
@@ -28,4 +48,16 @@ public class ChecklistData {
     public void setText(String text) {
         this.text = text;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeByte((byte) (checked ? 0x01 : 0x00));
+        dest.writeString(text);
+    }
+
 }
